c37b9ab62f71d3d2ccdfd95a01e268fce188cae9
“prabathwelivita” <Tharindu.prabath.welivita@gmail.com>
2024-05-06 17:14:58 +0530

    new one

---
+++ src/test/java/Pages/newclass.java
@@ -0,0 +1,11 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+public class newclass {
+
+	By txt_username = By.id("username");
+	By txt_password = By.id("password");
+    By btn_login =  By.id("submit");
+
+}
